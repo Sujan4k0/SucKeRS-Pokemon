e527cbdc242c03e7dc868c2aba903ee0e57e6d36
Sujan4k0 <sujan4k0@users.noreply.github.com>
2015-11-04 21:31:14 -0700

    Skeleton additions

--- src/model/Common.java
+++ src/model/Common.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.awt.image.BufferedImage;
+
 public class Common extends Pokemon {
+
+    public Common(String n, BufferedImage[] i, String t, double cp, double rp) {
+        super(n, i, t, cp, rp);
+        // TODO Auto-generated constructor stub
+    }
 
 }
