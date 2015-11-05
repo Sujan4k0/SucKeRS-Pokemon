694033a0566bbb6c123bd78f70fa8af1e6aa2939
browningsmith <browningsmith@email.arizona.edu>
2015-11-04 20:57:48 -0700

    added methods to Map class

--- src/model/Map.java
+++ src/model/Map.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Map {
+	
+	private Tile[][] board;
+	
+	private void createMap() {
+		
+		//TODO: Create a new two dimensional board of tiles and put that in 'this.board'
+	}
+	
+	public Tile[][] map() {
+		
+		return this.board;
+		
+	}
 
 }
