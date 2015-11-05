6570bf9de9e6df3c2110418c79aaa5976de84f7f
Sarina White <sarinarw@gmail.com>
2015-11-05 14:13:22 -0700

    skeleton methods added to GameMode, Ryan's email updated in commenting template

--- src/controller/GameMode.java
+++ src/controller/GameMode.java
@@ -1,5 +1,49 @@
+/*=========================================================================== 
+ | Assignment: FINAL PROJECT: [GameMode] 
+ | 
+ | Authors:    [Sujan Patel  (sujan4k0@email.arizona.edu)] 
+ |	     	   [Keith Smith  (browningsmith@email.arizona.edu)]
+ |	     	   [Ryan Kaye    (kayeryan1@email.arizona.edu)]
+ |             [Sarina White (sarinarw@email.arizona.edu)]
+ | 
+ | Course: 335 
+ | Instructor: Mercer
+ | Project Manager/Section Leader: Jeremy Mowery 
+ | Due Date: [12.7.15] 
+ | 
+ | Description: TODO Describe the class here. 
+ *===========================================================================*/
+
 package controller;
 
+import model.*;
+
 public class GameMode {
+	
+	Trainer trainer;
+	Map map;
+	
+	public GameMode(int mode) {
+		
+		
+		
+	}
+	
+	private boolean trainerCanMove() {
+		
+		return false;
+	}
+	
+	public boolean isGameWon() {
+		
+		return false;
+	}
+	
+	public boolean isGameLost() {
+		
+		return false;
+	}
+	
+	
 
 }
