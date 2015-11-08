0b8f4987220afcad995a14c75e2c2cf1325e7ab6
Sujan4k0 <sujan4k0@users.noreply.github.com>
2015-11-08 14:09:13 -0700

    Some GUI stuff.

--- src/view/PokemonGUI.java
+++ src/view/PokemonGUI.java
@@ -2,8 +2,19 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import controller.GameMode;
+import model.MapModel.MapType;
 
 public class PokemonGUI extends JFrame {
     
@@ -18,7 +29,6 @@ public class PokemonGUI extends JFrame {
 
 		layoutGUI();
 		registerListeners();
-
 	}
 
 	private void layoutGUI() {
@@ -30,23 +40,66 @@ public class PokemonGUI extends JFrame {
 	    
 	    startScreen = new JFrame();
 	    startScreen.setVisible(true);
-	    startScreen.setSize(300, 300);
+	    startScreen.setSize(600, 300);
 	    startScreen.setTitle("Pokemon");
+	    
+	    JPanel logoHeader = new JPanel();
+	    BufferedImage logo = null;
+	    try {
+            logo = ImageIO.read(new File("./images/PokemonLogo.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	    JLabel logoLabel = new JLabel(new ImageIcon(logo));
+	    logoHeader.add(logoLabel);
+	    startScreen.add(logoHeader, BorderLayout.NORTH);
 	    
 	    JPanel startOptions = new JPanel(new FlowLayout());
 	    startOptions.setSize(100, 100);
 	    JButton gameMode1 = new JButton("Catch Em' All");
-	    JButton gameMode2 = new JButton("Maze Escape");
-	    
+	    JButton gameMode2 = new JButton("Maze Escape");	    
 	    startOptions.add(gameMode1);
-	    startOptions.add(gameMode2);
+	    startOptions.add(gameMode2);   
+	    startScreen.add(startOptions, BorderLayout.CENTER);    
 	    
-	    startScreen.add(startOptions, BorderLayout.CENTER);
+	    gameMode1.addActionListener(new GameMode1Selected());
+	    gameMode2.addActionListener(new GameMode2Selected());
+	    
+//	    JPanel descriptors = new JPanel();
+//	    JButton descriptions = new JButton("Game Mode Descriptions");
+//	    descriptors.add(descriptions);
+//	    startScreen.add(descriptors, BorderLayout.SOUTH);
 	    
 	}
+	
+	private class GameMode1Selected implements ActionListener {
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+            // communicate with GameMode
+            GameMode mode = new GameMode(MapType.CEA);
+            
+            startScreen.setVisible(false);
+            
+        }   
+	}
+	
+	private class GameMode2Selected implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+            // communicate with GameMode
+            GameMode mode = new GameMode(MapType.MAZE);
+            
+            startScreen.setVisible(false);
+            
+        }   
+    }
+
+	
 	private void registerListeners() {
-		//TODO add necessary Listeners
 
 	}
 
