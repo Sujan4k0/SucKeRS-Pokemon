d352144eb9cc2d221139c8906d7f3e205dc8df0c
Ryan Kaye <rkaye@email.arizona.edu>
2015-11-15 15:55:55 -0700

    Added more item test cases and comments

--- src/model/ItemModel/Item.java
+++ src/model/ItemModel/Item.java
@@ -31,28 +31,50 @@ public abstract class Item implements Serializable{
 	/*---------------------------------------------------------------------
 	 |  Method name:    [getName]
 	 |  Purpose:  	    [return the name of an item]
-	 |  Parameters:     []
 	 |  Returns:  	    [A string representing the name of an item]
 	 *---------------------------------------------------------------------*/
 	public String getName(){
 		return name;
 	}
+	/*---------------------------------------------------------------------
+	 |  Method name:    [setName]
+	 |  Purpose:  	    [sets the name of an item]
+	 |  Parameters:     [String for the name]
+	 *---------------------------------------------------------------------*/
 	public void setName(String n){
 		name= n;
 	}
 
+	/*---------------------------------------------------------------------
+	 |  Method name:    [isForPokemon]
+	 |  Purpose:  	    [return whether item can be used on pokemon]
+	 |  Returns:  	    [boolean for above]
+	 *---------------------------------------------------------------------*/
 	public boolean isForPokemon() {
 		return forPokemon;
 	}
 
+	/*---------------------------------------------------------------------
+	 |  Method name:    [setForPokemon]
+	 |  Purpose:  	    [sets whether item can be used on pokemon]
+	 |  Parameters:  	[boolean for above]
+	 *---------------------------------------------------------------------*/
 	public void setForPokemon(boolean forPokemon) {
 		this.forPokemon = forPokemon;
 	}
-
+	/*---------------------------------------------------------------------
+	 |  Method name:    [isForTrainer]
+	 |  Purpose:  	    [return whether item can be used on Trainer]
+	 |  Returns:  	    [boolean for above]
+	 *---------------------------------------------------------------------*/
 	public boolean isForTrainer() {
 		return forTrainer;
 	}
-
+	/*---------------------------------------------------------------------
+	 |  Method name:    [setForTrainers]
+	 |  Purpose:  	    [sets whether item can be used on pokemon]
+	 |  Parameters:  	[boolean for above]
+	 *---------------------------------------------------------------------*/
 	public void setForTrainer(boolean forTrainer) {
 		this.forTrainer = forTrainer;
 	}
