55eeb41b52739b0b569f33a66deb5e52f0b06831
Sarina White <sarinarw@gmail.com>
2015-11-15 12:49:38 -0700

    changed certain Ground to GRASS type

--- src/model/MapModel/Ground.java
+++ src/model/MapModel/Ground.java
@@ -24,11 +24,11 @@ public enum Ground implements Tile {
 	ICE_5(TerrainType.ICE), CAVE_1(TerrainType.CAVE), CAVE_2(TerrainType.CAVE), CAVE_3(
 			TerrainType.CAVE), CAVE_4(TerrainType.CAVE), CAVE_5(TerrainType.CAVE), WEIRD_1(
 			TerrainType.MYSTERY), BINARY_1(TerrainType.MYSTERY), BINARY_2(TerrainType.MYSTERY),
-	BINARY_3(TerrainType.MYSTERY), T5(TerrainType.CAVE), GRASS_1(TerrainType.GRASS), GRASS_2(
-			TerrainType.GRASS), GRASS_3(TerrainType.GRASS), GRASS_4(TerrainType.GRASS), GRASS_5(
+	BINARY_3(TerrainType.MYSTERY), T5(TerrainType.CAVE), 
+	GRASS_1(TerrainType.GRASS), GRASS_2(TerrainType.GRASS), GRASS_3(TerrainType.GRASS), GRASS_4(TerrainType.GRASS), GRASS_5(
 			TerrainType.CAVE), SAND_1(TerrainType.DESERT), SAND_2(TerrainType.DESERT), SAND_3(
 			TerrainType.DESERT), SAND_4(TerrainType.DESERT), SAND_5(TerrainType.DESERT),
-	GRASS_1_2_HOR(TerrainType.GENERIC), GRASS_2_1_HOR(TerrainType.GENERIC), x3(TerrainType.CAVE),
+	GRASS_1_2_HOR(TerrainType.GRASS), GRASS_2_1_HOR(TerrainType.GRASS), x3(TerrainType.CAVE),
 	x4(TerrainType.CAVE), x5(TerrainType.CAVE);
 
 	private TerrainType terrainType;
