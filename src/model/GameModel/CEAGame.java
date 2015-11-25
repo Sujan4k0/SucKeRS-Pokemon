de9c950c5d88ac06d9e0c01719c458a759b286d8
Sujan4k0 <sujan4k0@users.noreply.github.com>
2015-11-25 09:41:47 -0700

    Stuff

--- src/model/GameModel/CEAGame.java
+++ src/model/GameModel/CEAGame.java
@@ -26,124 +26,124 @@ import view.*;
 
 public class CEAGame extends GameMode {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/*---------------------------------------------------------------------
-	 |  Method name:    [CEAGame]
-	 |  Purpose:  	    [Constructs a CEAGame (Catch 'em All)]
-	 |  Parameters:     [Random: for later random encounter/items/stuff]
-	 *---------------------------------------------------------------------*/
-	public CEAGame(Random rand) {
-		super(rand);
-		loadImages();
+    /*---------------------------------------------------------------------
+     |  Method name:    [CEAGame]
+     |  Purpose:  	    [Constructs a CEAGame (Catch 'em All)]
+     |  Parameters:     [Random: for later random encounter/items/stuff]
+     *---------------------------------------------------------------------*/
+    public CEAGame(Random rand) {
+        super(rand);
+        loadImages();
 
-		trainer.addItem(new Harmonica());
-		trainer.addItem(new Harmonica());
+        trainer.addItem(new Harmonica());
+        trainer.addItem(new Harmonica());
 
-		/*
-		 * for testing teleportation for (Pokemon p : database.getAllPokemon())
-		 * if (!p.getName().toUpperCase().equals("MEW")) trainer.addPokemon(p);
-		 */
+        //for testing teleportation 
+        for (Pokemon p : database.getAllPokemon())
+            if (!p.getName().toUpperCase().equals("MEW"))
+                trainer.addPokemon(p);
 
-	}
+    }
 
-	/*---------------------------------------------------------------------
-	 |  Method name:    [isGameWon]
-	 |  Purpose:  	    [To check if the user has won the game]
-	 |  Returns:  	    [boolean: true is user won, false is not]
-	 *---------------------------------------------------------------------*/
-	@Override
-	public boolean isGameWon() {
+    /*---------------------------------------------------------------------
+     |  Method name:    [isGameWon]
+     |  Purpose:  	    [To check if the user has won the game]
+     |  Returns:  	    [boolean: true is user won, false is not]
+     *---------------------------------------------------------------------*/
+    @Override
+    public boolean isGameWon() {
 
-		for (Pokemon p : trainer.getPokemon()) {
-			if (p.getName().equals("MEW"))
-				return true;
-		}
+        for (Pokemon p : trainer.getPokemon()) {
+            if (p.getName().equals("MEW"))
+                return true;
+        }
 
-		return false;
+        return false;
 
-	}
+    }
 
-	/*---------------------------------------------------------------------
-	 |  Method name:    [isGameLost]
-	 |  Purpose:  	    [To know if the user has lost the game]
-	 |  Returns:  	    [boolean: true if the user has lost, false if the user has not]
-	 *---------------------------------------------------------------------*/
-	@Override
-	public boolean isGameLost() {
-		// TODO add losing conditions
-		if (trainer.getSteps() == 0 || !trainer.getItems().contains(new PokeBall()))
-			return true;
+    /*---------------------------------------------------------------------
+     |  Method name:    [isGameLost]
+     |  Purpose:  	    [To know if the user has lost the game]
+     |  Returns:  	    [boolean: true if the user has lost, false if the user has not]
+     *---------------------------------------------------------------------*/
+    @Override
+    public boolean isGameLost() {
+        // TODO add losing conditions
+        if (trainer.getSteps() == 0 || !trainer.getItems().contains(new PokeBall()))
+            return true;
 
-		return false;
-	}
+        return false;
+    }
 
-	/*---------------------------------------------------------------------
-	 |  Method name:    [createMap]
-	 |  Purpose:  	    [assigns a CEAMap to the Map instance variable]
-	 *---------------------------------------------------------------------*/
-	@Override
-	public void createMap() {
-		map = new CEAMap(r);
-	}
+    /*---------------------------------------------------------------------
+     |  Method name:    [createMap]
+     |  Purpose:  	    [assigns a CEAMap to the Map instance variable]
+     *---------------------------------------------------------------------*/
+    @Override
+    public void createMap() {
+        map = new CEAMap(r);
+    }
 
-	/*---------------------------------------------------------------------
-	|  Method name:    [startEncounter]
-	|  Purpose:        [This start an encounter]
-	 *---------------------------------------------------------------------*/
-	@Override
-	public void startEncounter() {
+    /*---------------------------------------------------------------------
+    |  Method name:    [startEncounter]
+    |  Purpose:        [This start an encounter]
+     *---------------------------------------------------------------------*/
+    @Override
+    public void startEncounter() {
 
-		if (encounteredPokemon == null) {
-			// only uncommon and common pokemon can be encountered
-			// and only not in secrety secret land
-			if (trainer.getPoint().x >= Map.HEIGHT || trainer.getPoint().y >= Map.WIDTH) {
-				int rand = r.nextInt(5);
-				if (rand == 4) {
+        if (encounteredPokemon == null) {
+            // only uncommon and common pokemon can be encountered
+            // and only not in secrety secret land
+            if (trainer.getPoint().x >= Map.HEIGHT || trainer.getPoint().y >= Map.WIDTH) {
+                int rand = r.nextInt(5);
+                if (rand == 4) {
 
-					Point tp = trainer.getPoint();
+                    Point tp = trainer.getPoint();
 
-					// in plainy area
-					if (tp.x >= Map.HEIGHT && tp.x < Map.HEIGHT * 2 && tp.y < Map.WIDTH)
-						encounteredPokemon = database.getSteelix();
-					// in icey area
-					else if (tp.x >= Map.HEIGHT * 2 && tp.x < Map.HEIGHT * 3 && tp.y >= Map.WIDTH
-							&& tp.y < 2 * Map.WIDTH)
-						encounteredPokemon = database.getPikachu();
-					// in deserty area
-					else if (tp.x >= Map.HEIGHT && tp.x < Map.HEIGHT * 2 && tp.y >= Map.WIDTH * 2
-							&& tp.y < 3 * Map.WIDTH)
-						encounteredPokemon = database.getGyrados();
-					// in cavey area
-					else if (tp.x < Map.HEIGHT && tp.y >= Map.WIDTH && tp.y < 2 * Map.WIDTH)
-						encounteredPokemon = database.getExeggutor();
+                    // in plainy area
+                    if (tp.x >= Map.HEIGHT && tp.x < Map.HEIGHT * 2 && tp.y < Map.WIDTH)
+                        encounteredPokemon = database.getSteelix();
+                    // in icey area
+                    else if (tp.x >= Map.HEIGHT * 2 && tp.x < Map.HEIGHT * 3 && tp.y >= Map.WIDTH
+                            && tp.y < 2 * Map.WIDTH)
+                        encounteredPokemon = database.getPikachu();
+                    // in deserty area
+                    else if (tp.x >= Map.HEIGHT && tp.x < Map.HEIGHT * 2 && tp.y >= Map.WIDTH * 2
+                            && tp.y < 3 * Map.WIDTH)
+                        encounteredPokemon = database.getGyrados();
+                    // in cavey area
+                    else if (tp.x < Map.HEIGHT && tp.y >= Map.WIDTH && tp.y < 2 * Map.WIDTH)
+                        encounteredPokemon = database.getExeggutor();
 
-				} else
-					encounteredPokemon = database.getRandomCommon(map.getCurrentTerrain());
-			}
-		}
-		battleMessage = "You've encountered a " + encounteredPokemon.getName() + "!";
+                } else
+                    encounteredPokemon = database.getRandomCommon(map.getCurrentTerrain());
+            }
+        }
+        battleMessage = "You've encountered a " + encounteredPokemon.getName() + "!";
 
-		inBattle = true;
-		encounter.startEncounter(encounteredPokemon);
-		map.pauseBGMusic();
+        inBattle = true;
+        encounter.startEncounter(encounteredPokemon);
+        map.pauseBGMusic();
 
-	}
+    }
 
-	@Override
-	public void trainerCaughtPokemon(Pokemon p) {
-		// nothing hurr
-	}
+    @Override
+    public void trainerCaughtPokemon(Pokemon p) {
+        // nothing hurr
+    }
 
-	public void startLegEncounter() {
+    public void startLegEncounter() {
 
-		battleMessage = "You've encountered MEW!!\n AHHHHHHhhHHHhHhH.\n" + "Can you catch it?!?!";
+        battleMessage = "You've encountered MEW!!\n AHHHHHHhhHHHhHhH.\n" + "Can you catch it?!?!";
 
-		inBattle = true;
-		encounteredPokemon = database.getMew();
-		encounter.startEncounter(encounteredPokemon);
-		map.pauseBGMusic();
+        inBattle = true;
+        encounteredPokemon = database.getMew();
+        encounter.startEncounter(encounteredPokemon);
+        map.pauseBGMusic();
 
-	}
+    }
 
 }
